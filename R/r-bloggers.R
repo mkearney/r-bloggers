82b7d6a37b039b1a1dccea2b391c18c86fa0c2d0
Michael Kearney <mkearney@ku.edu>
2017-01-24 17:48:24 -0600

    added master list and fixed chr limit

--- R/r-bloggers.R
+++ R/r-bloggers.R
@@ -34,9 +34,11 @@ newest <- file.path("/Users/mwk/r/r-bloggers/data", newest)
 previous <- readRDS(newest)
 
 ## post tweet
-if (!all(links %in% previous)) {
+rblinks <- readRDS("/Users/mwk/r/r-bloggers/data/rbloggers_links.rds")
+if (!all(links %in% rblinks)) {
     token <- readRDS("/Users/mwk/rtw.rds")
-    url2tweet <- links[!links %in% previous]
+    url2tweet <- links[!links %in% rblinks]
+    yesno <- vector("logical", length(url2tweet))
     for (i in url2tweet) {
         txt2tweet <- i %>%
             gsub("(^https://www.r-bloggers.com/)|(/)",
@@ -50,25 +52,39 @@ if (!all(links %in% previous)) {
                 .[nchar(.) %>% cumsum() < 40]
             txt2tweet <- paste(txt2tweet, collapse = " ")
             txt2tweet <- paste0(txt2tweet, "...")
-            if ((nchar(txt2tweet) + nchar(i) + 12) > 140) {
-                txt2tweet <- strsplit(txt2tweet, " ")[[1]]
-                txt2tweet <- txt2tweet %>%
-                    .[nchar(.) %>% cumsum() < (127 - nchar(i))]
-                txt2tweet <- paste(txt2tweet, collapse = " ")
-                txt2tweet <- paste0(txt2tweet, "...")
-            }
+            ##if ((nchar(txt2tweet) + nchar(i) + 12) > 140) {
+            ##    txt2tweet <- strsplit(txt2tweet, " ")[[1]]
+            ##    txt2tweet <- txt2tweet %>%
+            ##        .[nchar(.) %>% cumsum() < (127 - nchar(i))]
+            ##    txt2tweet <- paste(txt2tweet, collapse = " ")
+            ##    txt2tweet <- paste0(txt2tweet, "...")
+            ##}
         }
-        post_tweet(paste0(txt2tweet, " #rstats ", i),
-                   token = token)
+        pt <- post_tweet(
+            paste0(txt2tweet, " #rstats ", i),
+            token = token)
+        pt <- httr::GET("http://mikewk.com")
+        if (isTRUE(pt$all_headers[[1]][["status"]] == 200)) {
+            yesno[i] <- TRUE
+        } else {
+            yesno[i] <- FALSE
+        }
     }
-}
-## save new file
-spacetime <- paste0(format(
-    Sys.time(), "%m-%d-%y-%H-%M",
-    tz = "America/Chicago"),
-    ".rds")
-save_as <- file.path("/Users/mwk/r/r-bloggers/data",
-                     spacetime)
 
-## save file
-saveRDS(links, save_as)
+    ## save new file
+    spacetime <- paste0(format(
+        Sys.time(), "%m-%d-%y-%H-%M",
+        tz = "America/Chicago"),
+        ".rds")
+    save_as <- file.path("/Users/mwk/r/r-bloggers/data",
+                         spacetime)
+    saveRDS(links, save_as)
+
+    ## save comprehensive link file (if tweet was successful)
+    rblinks <- c(rblinks, links[yesno])
+    saveRDS(rblinks,
+            "/Users/mwk/r/r-bloggers/data/rbloggers_links.rds")
+
+} else {
+    message("Sorry, there are no new R-bloggers.com posts to tweet!")
+}
